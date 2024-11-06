31505bcd3a4bfec8a71e2ba5c9ca5f99f57fed48
Fernando Aldama <fer.ald87@gmail.com>
2024-11-06 18:56:40 +0100

    ignorar ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true // Ignora errores de ESLint durante la construcción
+  }
+  // otras opciones de configuración
+}
 
-export default nextConfig;
+export default nextConfig
